f39f46884cd3caba8cd6abf69b6f227e8ea1d86a
Durga Meenakshi <chdurgameenakshi@gmail.com>
2026-06-11 14:33:44 +0530

    Updated App.jsx

--- My_project/src/App.jsx
+++ My_project/src/App.jsx
@@ -1,20 +1,11 @@
 
 import './App.css'
+import Header from './Header'
 function App() {
 
 return (
   <>
   <section style={{ textAlign: "center" }}>
-  <header style={{display: "flex",justifyContent:"center",justifyContent:"space-evenly"}}> 
-   <img height={30} width={100}  src="https://zerodha.com/static/images/logo.svg" alt="logo" />
-   <nav style={{display:"flex",gap:"20px",alignItems:"center"}}>
-   <a href="">SignUp</a>
-   <a href="">About</a>
-   <a href="">Pricing</a>
-   <a href="">Products</a>
-   <a href="">Support</a>
-   </nav>
-   </header>
    <img  src="https://zerodha.com/static/images/landing.svg" alt=""/>
    <h2>Invest in everything</h2>
    <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.
